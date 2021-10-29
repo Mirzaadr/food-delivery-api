7def1318e754db6217888e60b3f27768a75c770a
Mirzaadr <mirzaadr34@yahoo.com>
2021-10-29 21:26:50 +0700

    add test

--- src/server.js
+++ src/server.js
@@ -1,3 +1,25 @@
-console.log('Server up and running');
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
+});
+
+const server = app.listen(port, () => {
+  console.log(`Live at ${server.address().port}`);
+});
+
+export default app;
