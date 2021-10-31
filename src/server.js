c8961ba82ef3ba779bb293ab588664af94a6a981
Mirzaadr <mirzaadr34@yahoo.com>
2021-10-31 18:42:29 +0700

    add sign up

--- src/server.js
+++ src/server.js
@@ -4,10 +4,14 @@ import cors from 'cors';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 
+import routes from './routes';
+
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+
+console.log(`Company Config : Using Environment "${process.env.NODE_ENV}"`)
 
 app.use(cors());
 app.use(morgan('dev'));
@@ -17,6 +21,8 @@ app.use(bodyParser.json());
 app.get('/test', (req, res, next) => {
   res.status(200).send("Welcome");
 });
+
+app.use(routes);
 
 app.use((req, res) => {
   res.status(404).json({ error: 'Route not found' });
